7b78a21d0caa79c87f5a4740d7d14b35ee637faf
bmj0053 <bmj05378@gmail.com>
2023-11-11 23:20:19 +0900

    feat: 뱃지 상수 추가

--- src/constants/StandartConstant.js
+++ src/constants/StandartConstant.js
@@ -1,5 +1,17 @@
 const STANDARD = Object.freeze({
   GIVE_CHAMPAGNE: 120000,
+  BADGE_SANTA: {
+    PRICE: 20000,
+    NAME: "산타"
+  },
+  BADGE_TREE: {
+    PRICE: 10000,
+    NAME: "트리"
+  },
+  BADGE_STAR: {
+    PRICE: 5000,
+    NAME: "별"
+  },
 })
 
 export default STANDARD;
