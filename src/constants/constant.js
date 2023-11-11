e967223cce45045481fa1c3e9684acbbda678321
bmj0053 <bmj05378@gmail.com>
2023-11-11 21:40:23 +0900

    fix: 상수 형식 변경

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,60 +1,110 @@
 const MENU = Object.freeze({
-  APPETIZER: [
-    {
-      MENU_NAME: '양송이수프',
-      MENU_PRICE: '6000'
-    },
-    {
-      MENU_NAME: '타파스',
-      MENU_PRICE: '5500'
-    },
-    {
-      MENU_NAME: '시저샐러드',
-      MENU_PRICE: '8000'
-    },
-  ],
-  MAIN: [
-    {
-      MENU_NAME: '티본스테이크',
-      MENU_PRICE: '55000'
-    },
-    {
-      MENU_NAME: '바비큐립',
-      MENU_PRICE: '54000'
-    },
-    {
-      MENU_NAME: '해산물파스타',
-      MENU_PRICE: '35000'
-    },
-    {
-      MENU_NAME: '크리스마스파스타',
-      MENU_PRICE: '25000'
-    },
-  ],
-  DESSERT: [
-    {
-      MENU_NAME: '초코케이크',
-      MENU_PRICE: '15000'
-    },
-    {
-      MENU_NAME: '아이스크림',
-      MENU_PRICE: '5000'
-    },
-  ],
-  DRINK: [
-    {
-      MENU_NAME: '제로콜라',
-      MENU_PRICE: '3000'
-    },
-    {
-      MENU_NAME: '레드와인',
-      MENU_PRICE: '60000'
-    },
-    {
-      MENU_NAME: '샴페인',
-      MENU_PRICE: '25000'
-    },
-  ]
+  '양송이수프': {
+    TYPE: 'appetizer',
+    PRICE: 6000
+  },
+  '타파스': {
+    TYPE: 'appetizer',
+    PRICE: 5500
+  },
+  '시저샐러드': {
+    TYPE: 'appetizer',
+    PRICE: 8000
+  },
+  '티본스테이크': {
+    TYPE: 'main',
+    PRICE: 55000
+  },
+  '바비큐립': {
+    TYPE: 'main',
+    PRICE: 54000
+  },
+  '해산물파스타': {
+    TYPE: 'main',
+    PRICE: 35000
+  },
+  '크리스마스파스타': {
+    TYPE: 'main',
+    PRICE: 25000
+  },
+  '초코케이크': {
+    TYPE: 'dessert',
+    PRICE: 15000
+  },
+  '아이스크림': {
+    TYPE: 'dessert',
+    PRICE: 5000
+  },
+  '제로콜라': {
+    TYPE: 'drink',
+    PRICE: 3000
+  },
+  '레드와인': {
+    TYPE: 'drink',
+    PRICE: 60000
+  },
+  '샴페인': {
+    TYPE: 'drink',
+    PRICE: 25000
+  },
 })
+// const MENU = Object.freeze({
+//   APPETIZER: [
+//     {
+//       MENU_NAME: '양송이수프',
+//       MENU_PRICE: '6000'
+//     },
+//     {
+//       MENU_NAME: '타파스',
+//       MENU_PRICE: '5500'
+//     },
+//     {
+//       MENU_NAME: '시저샐러드',
+//       MENU_PRICE: '8000'
+//     },
+//   ],
+//   MAIN: [
+//     {
+//       MENU_NAME: '티본스테이크',
+//       MENU_PRICE: '55000'
+//     },
+//     {
+//       MENU_NAME: '바비큐립',
+//       MENU_PRICE: '54000'
+//     },
+//     {
+//       MENU_NAME: '해산물파스타',
+//       MENU_PRICE: '35000'
+//     },
+//     {
+//       MENU_NAME: '크리스마스파스타',
+//       MENU_PRICE: '25000'
+//     },
+//   ],
+//   DESSERT: [
+//     {
+//       MENU_NAME: '초코케이크',
+//       MENU_PRICE: '15000'
+//     },
+//     {
+//       MENU_NAME: '아이스크림',
+//       MENU_PRICE: '5000'
+//     },
+//   ],
+//   DRINK: [
+//     {
+//       MENU_NAME: '제로콜라',
+//       MENU_PRICE: '3000'
+//     },
+//     {
+//       MENU_NAME: '레드와인',
+//       MENU_PRICE: '60000'
+//     },
+//     {
+//       MENU_NAME: '샴페인',
+//       MENU_PRICE: '25000'
+//     },
+//   ]
+// })
 
 export default MENU;
