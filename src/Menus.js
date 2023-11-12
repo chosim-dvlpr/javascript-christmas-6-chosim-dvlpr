051ddd5ad7060ce394c15edf174ad79294379593
bmj0053 <bmj05378@gmail.com>
2023-11-12 13:25:53 +0900

    feat: 메뉴 유효성 검사 추가

--- src/Menus.js
+++ src/Menus.js
@@ -17,26 +17,44 @@ class Menus {
     }
 
     const menu = menus.split(',').map(String)
+    let menuNum = 0;
+    let isOnlyDrinks = true;
     for (let i = 0; i < menu.length; i++) {
       const element = menu[i];
+
       // dash 확인
       const dash = element.indexOf('-')
       if (dash === -1) {
         throw new Error(ERROR_MESSAGE.INPUT_MENU);
       };
+      
       // 마지막이 숫자인지 확인
-      const lastNum = element.substr(-1);
+      const lastNum = element.substr(dash+1, element.length);
       if (isNaN(lastNum)) {
         throw new Error(ERROR_MESSAGE.INPUT_MENU);
       };
+
       // 메뉴 목록에 있는 메뉴인지 확인
       const menuName = element.substr(0, dash);
       if (MENU[menuName] === undefined) {
         throw new Error(ERROR_MESSAGE.INPUT_MENU);
       };
+
+      // 메뉴가 음료가 아닌지 확인
+      if (MENU[menuName].TYPE !== 'drink') {
+        isOnlyDrinks = false;
+      };
+
+      // 메뉴 20개 초과인지 확인
+      menuNum = menuNum + Number(lastNum);
+      if (menuNum > 20) {
+        throw new Error(ERROR_MESSAGE.INPUT_MENU);
+      };
     }
 
-    return menu
+    if (isOnlyDrinks) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
   }
 }
 
