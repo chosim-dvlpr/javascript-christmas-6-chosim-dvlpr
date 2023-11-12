c6ad31c6e9fdb215e81af1cfb20c606a36d41f20
bmj0053 <bmj05378@gmail.com>
2023-11-12 13:01:01 +0900

    test: 메뉴 입력값 테스트 추가

--- __tests__/MenusTest.js
+++ __tests__/MenusTest.js
@@ -1,9 +1,15 @@
 import Menus from "../src/Menus.js";
+import ERROR_MESSAGE from "../src/constants/ErrorConstant.js";
 
 describe("메뉴 클래스 테스트", () => {
   test("메뉴 입력값에 -가 없다면 예외가 발생한다", () => {
     expect(() => {
-      new Menus("파스타-1개, 레드와인-2개");
-    }).toThrow("[ERROR]");
+      new Menus("해산물파스타, 레드와인2");
+    }).toThrow(ERROR_MESSAGE.INPUT_MENU);
+  });
+  test("메뉴 입력값 마지막이 숫자가 아니라면 예외가 발생한다", () => {
+    expect(() => {
+      new Menus("해산물파스타-1, 레드와인-a");
+    }).toThrow(ERROR_MESSAGE.INPUT_MENU);
   });
 });
