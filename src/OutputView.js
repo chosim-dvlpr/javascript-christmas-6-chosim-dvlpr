d2a69199a59867afda7b7c09b582861bfcad89b2
bmj0053 <bmj05378@gmail.com>
2023-11-13 22:19:02 +0900

    refactor: 할인 전 총주문 금액 분리

--- src/OutputView.js
+++ src/OutputView.js
@@ -20,30 +20,15 @@ const OutputView = {
   },
   printBeforeDCTotal(inputMenuList) {
     Console.print("<할인 전 총주문 금액>");
-    let totals = 0;
 
+    let totals = 0;
     let mainTotals = 0;
     let mainCounts = 0;
-
     let dessertTotals = 0;
     let dessertCounts = 0;
+    
+    totals = this.calcTotals(inputMenuList, totals, mainTotals, mainCounts, dessertTotals, dessertCounts);
 
-    for (let i = 0; i < inputMenuList.length; i++) {
-      const inputMenuListElement = inputMenuList[i];
-
-      totals = totals + MENU[inputMenuListElement.menuName].PRICE * inputMenuListElement.menuNum
-      
-      switch (MENU[inputMenuListElement.menuName].TYPE) {
-        case 'main':
-          mainTotals = mainTotals + MENU[inputMenuListElement.menuName].PRICE  
-          mainCounts = mainCounts + Number(inputMenuListElement.menuNum);
-          break;
-        case 'dessert':
-          dessertTotals = dessertTotals + MENU[inputMenuListElement.menuName].PRICE  
-          dessertCounts = dessertCounts + Number(inputMenuListElement.menuNum);
-          break;
-      }
-    }
     const stringTotals = totals.toLocaleString();
     Console.print(`${stringTotals}원\n`);
     return {totals, dessertCounts, mainCounts}
@@ -158,7 +143,25 @@ const OutputView = {
       Console.print(`${menuName} ${menuNum}개`);
     };
     return inputMenuListSplited
-  }
+  },
+  calcTotals(inputMenuList, totals, mainTotals, mainCounts, dessertTotals, dessertCounts) {
+    for (let i = 0; i < inputMenuList.length; i++) {
+      const inputMenuListElement = inputMenuList[i];
+
+      totals = totals + MENU[inputMenuListElement.menuName].PRICE * inputMenuListElement.menuNum
+      switch (MENU[inputMenuListElement.menuName].TYPE) {
+        case 'main':
+          mainTotals = mainTotals + MENU[inputMenuListElement.menuName].PRICE  
+          mainCounts = mainCounts + Number(inputMenuListElement.menuNum);
+          break;
+        case 'dessert':
+          dessertTotals = dessertTotals + MENU[inputMenuListElement.menuName].PRICE  
+          dessertCounts = dessertCounts + Number(inputMenuListElement.menuNum);
+          break;
+      }
+    }
+    return totals
+  },
 }
 
 export default OutputView;
