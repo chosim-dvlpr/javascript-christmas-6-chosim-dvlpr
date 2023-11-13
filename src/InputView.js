06316a2158230c264dd034a55147ebb251cacc4d
bmj0053 <bmj05378@gmail.com>
2023-11-13 21:58:23 +0900

    refactor: 변수 선언 위치 변경

--- src/InputView.js
+++ src/InputView.js
@@ -4,9 +4,8 @@ import Menus from './Menus.js';
 
 const InputView = {
   async readDate() {
-    let input;
     try {
-      input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+      const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
       new Dates(input);
       return input
     } catch (error) {
