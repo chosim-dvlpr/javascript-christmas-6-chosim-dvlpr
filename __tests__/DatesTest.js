fe23f5551b412d77e7ee983e241093321b6206a0
bmj0053 <bmj05378@gmail.com>
2023-11-13 21:50:16 +0900

    test: date 테스트 함수 추가

--- __tests__/DatesTest.js
+++ __tests__/DatesTest.js
@@ -1,9 +1,20 @@
 import Dates from "../src/Dates.js";
+import ERROR_MESSAGE from "../src/constants/ErrorConstant.js";
 
 describe("Dates 클래스 테스트", () => {
   test("날짜 입력값이 숫자가 아니라면 예외가 발생한다", () => {
     expect(() => {
       new Dates('1k');
-    }).toThrow("[ERROR]");
+    }).toThrow(ERROR_MESSAGE.INPUT_DATE);
+  });
+  test("날짜 입력값이 공백이라면 예외가 발생한다", () => {
+    expect(() => {
+      new Dates('');
+    }).toThrow(ERROR_MESSAGE.INPUT_DATE);
+  });
+  test("날짜 입력값이 1 이상 31 이하가 아니라면 예외가 발생한다", () => {
+    expect(() => {
+      new Dates([1, 31, 0]);
+    }).toThrow(ERROR_MESSAGE.INPUT_DATE);
   });
 });
