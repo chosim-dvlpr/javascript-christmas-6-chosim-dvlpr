74bc38eedc4f2a00cb0519dd4ebaa44d221b2898
bmj0053 <bmj05378@gmail.com>
2023-11-14 21:01:36 +0900

    refactor: beforeDCView 생성 및 함수 분리

---
+++ src/view/BeforeDCView.js
@@ -0,0 +1,40 @@
+import { Console } from '@woowacourse/mission-utils'
+import MENU from '../constants/MenuConstant';
+
+const BeforeDCView = {
+  printBeforeDCTotal(inputMenuList) {
+    Console.print("<할인 전 총주문 금액>");
+
+    let totals = 0;
+    let mainTotals = 0;
+    let mainCounts = 0;
+    let dessertTotals = 0;
+    let dessertCounts = 0;
+
+    ({totals, mainTotals, mainCounts, dessertTotals, dessertCounts} = this.calcTotals(inputMenuList, totals, mainTotals, mainCounts, dessertTotals, dessertCounts));
+    const stringTotals = totals.toLocaleString();
+    Console.print(`${stringTotals}원\n`);
+    return {totals, dessertCounts, mainCounts}
+  },
+  
+  calcTotals(inputMenuList, totals, mainTotals, mainCounts, dessertTotals, dessertCounts) {
+    for (let i = 0; i < inputMenuList.length; i++) {
+      const inputMenuListElement = inputMenuList[i];
+
+      totals = totals + MENU[inputMenuListElement.menuName].PRICE * inputMenuListElement.menuNum
+      switch (MENU[inputMenuListElement.menuName].TYPE) {
+        case 'main':
+          mainTotals = mainTotals + MENU[inputMenuListElement.menuName].PRICE  
+          mainCounts = mainCounts + Number(inputMenuListElement.menuNum);
+          break;
+        case 'dessert':
+          dessertTotals = dessertTotals + MENU[inputMenuListElement.menuName].PRICE  
+          dessertCounts = dessertCounts + Number(inputMenuListElement.menuNum);
+          break;
+      }
+    }
+    return {totals, mainTotals, mainCounts, dessertTotals, dessertCounts}
+  },
+}
+
+export default BeforeDCView;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -18,20 +18,7 @@ const OutputView = {
   printBenefitsMessage(date) {
     Console.print(`12월 ${date}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
   },
-  // printBeforeDCTotal(inputMenuList) {
-  //   Console.print("<할인 전 총주문 금액>");
 
-  //   let totals = 0;
-  //   let mainTotals = 0;
-  //   let mainCounts = 0;
-  //   let dessertTotals = 0;
-  //   let dessertCounts = 0;
-
-  //   ({totals, mainTotals, mainCounts, dessertTotals, dessertCounts} = this.calcTotals(inputMenuList, totals, mainTotals, mainCounts, dessertTotals, dessertCounts));
-  //   const stringTotals = totals.toLocaleString();
-  //   Console.print(`${stringTotals}원\n`);
-  //   return {totals, dessertCounts, mainCounts}
-  // },
   printGivingMenu(totals) {
     Console.print("<증정 메뉴>");
     if (totals > STANDARD.GIVE_CHAMPAGNE) {
@@ -120,24 +107,6 @@ const OutputView = {
     };
     return inputMenuListSplited
   },
-  // calcTotals(inputMenuList, totals, mainTotals, mainCounts, dessertTotals, dessertCounts) {
-  //   for (let i = 0; i < inputMenuList.length; i++) {
-  //     const inputMenuListElement = inputMenuList[i];
-
-  //     totals = totals + MENU[inputMenuListElement.menuName].PRICE * inputMenuListElement.menuNum
-  //     switch (MENU[inputMenuListElement.menuName].TYPE) {
-  //       case 'main':
-  //         mainTotals = mainTotals + MENU[inputMenuListElement.menuName].PRICE  
-  //         mainCounts = mainCounts + Number(inputMenuListElement.menuNum);
-  //         break;
-  //       case 'dessert':
-  //         dessertTotals = dessertTotals + MENU[inputMenuListElement.menuName].PRICE  
-  //         dessertCounts = dessertCounts + Number(inputMenuListElement.menuNum);
-  //         break;
-  //     }
-  //   }
-  //   return {totals, mainTotals, mainCounts, dessertTotals, dessertCounts}
-  // },
   christmasEvent(dates, totalDC) {
     let christmasDC = 0;
     let dDayDC = 0;
