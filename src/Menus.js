c3d40feb342de38083b702318077b16967bc8fb6
bmj0053 <bmj05378@gmail.com>
2023-11-14 21:52:12 +0900

    refactor: Menus 리팩터링

--- src/Menus.js
+++ src/Menus.js
@@ -10,51 +10,87 @@ class Menus {
   }
 
   #validate(menus) {
-    if (menus === null) {
-      throw new Error(ERROR_MESSAGE.INPUT_MENU);
-    }
+    this.#validateNull(menus);
 
     const menuItems = menus.split(',').map(item => item.trim());
     let totalMenuNum = 0;
-    let isOnlyDrinks = true;
+    let notIncludeDrinks = 0;
     let checkMenus = [];
 
     for (const item of menuItems) {
-      const dash = item.indexOf('-');
+      const { menuName, menuCount } = this.#menuDetail(item);
       
-      if (dash === -1) {
-        throw new Error(ERROR_MESSAGE.INPUT_MENU);
-      }
-      
-      const menuName = item.substring(0, dash);
-      const menuCount = parseInt(item.substr(dash+1));
-      
-      if (isNaN(menuCount) || menuCount < 1) {
-        throw new Error(ERROR_MESSAGE.INPUT_MENU);
-      }
-      
-      if (!MENU[menuName]) {
-        throw new Error(ERROR_MESSAGE.INPUT_MENU);
-      }
-
-      if (checkMenus.includes(menuName)) {
-        throw new Error(ERROR_MESSAGE.INPUT_MENU);
-      }
-      checkMenus.push(menuName);
-
-      if (MENU[menuName].TYPE !== 'drink') {
-        isOnlyDrinks = false;
-      }
+      this.#validateDash(item);
+      this.#validateMenuCount(menuCount);
+      this.#validateMenu(menuName);
+      checkMenus = this.#validateDuplicateMenu(checkMenus, menuName);
 
       totalMenuNum += menuCount;
 
-      if (totalMenuNum > 20) {
-        throw new Error(ERROR_MESSAGE.INPUT_MENU);
-      }
+      this.#validateTotalMenuNum(totalMenuNum);
+      notIncludeDrinks = this.#updateIsOnlyDrinks(notIncludeDrinks, menuName);
+    }
+    this.#validateIsOnlyDrinks(notIncludeDrinks);
+  }
 
-      if (isOnlyDrinks) {
-        throw new Error(ERROR_MESSAGE.INPUT_MENU);
-      }
+  #validateNull(menus) {
+    if (menus === null) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
+  }
+
+  #menuDetail(item) {
+    const dash = item.indexOf('-');
+
+    return {
+      menuName: item.substr(0, dash),
+      menuCount: parseInt(item.substr(dash+1))
+    }
+  }
+
+  #validateDash(item) {
+    const dash = item.indexOf('-');
+    if (dash === -1) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
+  }
+
+  #validateMenuCount(menuCount) {
+    if (isNaN(menuCount) || menuCount < 1) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
+  }
+
+  #validateMenu(menuName) {
+    if (!MENU[menuName]) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
+  }
+
+  #validateDuplicateMenu(checkMenus, menuName) {
+    if (checkMenus.includes(menuName)) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
+    checkMenus.push(menuName);
+    return checkMenus
+  }
+
+  #validateTotalMenuNum(totalMenuNum) {
+    if (totalMenuNum > 20) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
+    }
+  }
+
+  #updateIsOnlyDrinks(notIncludeDrinks, menuName) {
+    if (MENU[menuName].TYPE !== 'drink') {
+      notIncludeDrinks++;
+    }
+    return notIncludeDrinks
+  }
+
+  #validateIsOnlyDrinks(notIncludeDrinks) {
+    if (notIncludeDrinks === 0) {
+      throw new Error(ERROR_MESSAGE.INPUT_MENU);
     }
   }
 }
